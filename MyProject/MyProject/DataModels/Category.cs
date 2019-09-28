a21c9e2f14ac7d7561164ea78767ac40852a9df7
Hien <hyhien@gmail.com>
2019-09-28 15:44:30 +0700

    Add CategoryMenu view component

---
+++ MyProject/MyProject/DataModels/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.DataModels
+{
+    public class Category
+    {
+        public int CaterogyId { get; set; }
+        public string CaterogyName { get; set; }
+    }
+}
---
+++ MyProject/MyProject/ViewComponents/CategoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.ViewComponents
+{
+    public class CategoryMenu : ViewComponent
+    {
+        /*Chỉ có 1 action duy nhất là Invoke()*/
+        public IViewComponentResult Invoke()
+        {
+            //Gải sử đã lấy được dữ liệu Category
+            var data = new List<Category>()
+            {
+                new Category{CaterogyId = 1, CaterogyName = "Bia"},
+                new Category{CaterogyId = 2, CaterogyName = "Nước ngọt"},
+                new Category{CaterogyId = 3, CaterogyName = "Điện máy"},
+            };
+
+            //return View(data);
+            return View("Default", data);
+        }
+    }
+}

29152ee1d321d2018e779abd57490937ebbdf9d3
Hien <hyhien@gmail.com>
2019-10-12 14:12:48 +0700

    Day 12 - Project Init

---
+++ D12_EFCore_DBFirst/EFCore_DBFirst/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore_DBFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCore_DBFirst.Controllers
+{
+    public class DemoController : Controller
+    {
+        private readonly MyeStoreContext _context;
+        public DemoController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string TuKhoa, double GiaTu, double GiaDen)
+        {
+            return View();
+        }
+    }
+}

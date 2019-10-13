ad137189da19bb84599895008f6b0a68fc8e55b6
Hien <hyhien@gmail.com>
2019-10-13 09:12:06 +0700

    Component NhaCungCap

---
+++ D13_EFCore_DBFirst/EFCore_DBFirst/ViewComponents/NhaCungCap.cs
@@ -0,0 +1,26 @@
+﻿using EFCore_DBFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore_DBFirst.ViewComponents
+{
+    public class NhaCungCap : ViewComponent
+    {
+        private readonly MyeStoreContext _context;
+        public NhaCungCap(MyeStoreContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var data = _context.NhaCungCap
+                .Where(lo => lo.HangHoa.Count() > 0)
+                .OrderBy(lo => lo.TenCongTy);
+            return View(data);
+        }
+    }
+}

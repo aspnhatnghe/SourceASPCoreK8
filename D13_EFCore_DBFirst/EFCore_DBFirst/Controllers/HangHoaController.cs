9392c0ec303d5514507a930a334162701ac3048e
Hien <hyhien@gmail.com>
2019-10-13 11:36:18 +0700

    Sunmit

--- D13_EFCore_DBFirst/EFCore_DBFirst/Controllers/HangHoaController.cs
+++ D13_EFCore_DBFirst/EFCore_DBFirst/Controllers/HangHoaController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using EFCore_DBFirst.Models;
 using EFCore_DBFirst.ViewModels;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EFCore_DBFirst.Controllers
@@ -30,6 +31,20 @@ namespace EFCore_DBFirst.Controllers
                 data = data.Where(p => p.MaNcc == ncc).AsQueryable();
             }
             var result = data.Skip((page - 1) * MyTools.SoSanPham1Trang).Take(MyTools.SoSanPham1Trang).ToList();
+
+            ViewBag.TongSoTrang = (int)Math.Ceiling(data.Count() * 1.0 / MyTools.SoSanPham1Trang);
+            ViewBag.TrangHienTai = page;
+
+            var fullUrl = HttpContext.Request.GetEncodedUrl();
+
+            var host = HttpContext.Request.IsHttps ? "https" : "http" + "://" + HttpContext.Request.Host;
+            if(loai.HasValue)
+            {
+                host += "?loai=" + loai.Value;
+            }
+
+            ViewBag.loai = loai;
+            ViewBag.ncc = ncc;
 
             return View(_mapper.Map<List<HangHoaViewModel>>(result));
         }
---
+++ D13_EFCore_DBFirst/EFCore_DBFirst/Controllers/LoaiController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EFCore_DBFirst.Models;
+
+namespace EFCore_DBFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaiController : ControllerBase
+    {
+        private readonly MyeStoreContext _context;
+
+        public LoaiController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Loai
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Loai>>> GetLoai()
+        {
+            return await _context.Loai.ToListAsync();
+        }
+
+        // GET: api/Loai/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Loai>> GetLoai(int id)
+        {
+            var loai = _context.Loai
+                .Include(p => p.HangHoa)
+                .SingleOrDefault(p=> p.MaLoai == id);
+
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            return loai;
+        }
+
+        // PUT: api/Loai/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLoai(int id, Loai loai)
+        {
+            if (id != loai.MaLoai)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(loai).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoaiExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Loai
+        [HttpPost]
+        public async Task<ActionResult<Loai>> PostLoai(Loai loai)
+        {
+            _context.Loai.Add(loai);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLoai", new { id = loai.MaLoai }, loai);
+        }
+
+        // DELETE: api/Loai/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Loai>> DeleteLoai(int id)
+        {
+            var loai = await _context.Loai.FindAsync(id);
+            if (loai == null)
+            {
+                return NotFound();
+            }
+
+            _context.Loai.Remove(loai);
+            await _context.SaveChangesAsync();
+
+            return loai;
+        }
+
+        private bool LoaiExists(int id)
+        {
+            return _context.Loai.Any(e => e.MaLoai == id);
+        }
+    }
+}

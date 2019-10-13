daee9c12cb3c5e3e660145da8210593e079e2734
Hien <hyhien@gmail.com>
2019-10-13 08:51:54 +0700

    ViewComponent Loai

---
+++ D13_EFCore_DBFirst/EFCore_DBFirst/ViewComponents/Loai.cs
@@ -0,0 +1,23 @@
+﻿using EFCore_DBFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EFCore_DBFirst.ViewComponents
+{
+    public class LoaiViewComponent : ViewComponent
+    {
+        private readonly MyeStoreContext _context;
+        public LoaiViewComponent(MyeStoreContext ctx)
+        {
+            _context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var data = _context.Loai
+                .Where(lo => lo.HangHoa.Count() > 0)
+                .OrderBy(lo => lo.TenLoai);
+            return View(data);
+        }
+    }
+}
